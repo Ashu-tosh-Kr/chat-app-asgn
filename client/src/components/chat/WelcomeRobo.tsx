0235c7e3116910e60a0d6041454f0e44b20776be
Ashutosh Kumar <64960336+Ashu-tosh-Kr@users.noreply.github.com>
2023-07-11 17:37:35 +0530

    improve welcome screen

--- client/src/components/chat/WelcomeRobo.tsx
+++ client/src/components/chat/WelcomeRobo.tsx
@@ -1,5 +1,6 @@
 import Robot from "../../assets/robot.gif";
-import { Flex, Heading, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import Logo from "../../assets/logo.png";
 
 export default function Welcome() {
   const userName = JSON.parse(localStorage.getItem("user") as string).username;
@@ -11,10 +12,30 @@ export default function Welcome() {
       align={"center"}
       color={"brand.200"}
       w="70%"
+      pos={"relative"}
     >
+      <Box
+        backgroundImage="url(/chat-bg.png)"
+        opacity={0.1}
+        left={0}
+        top={0}
+        backgroundAttachment={"fixed"}
+        position={"absolute"}
+        w={"full"}
+        h={"full"}
+        overflow={"hidden"}
+      />
       <Image h={"20rem"} src={Robot} alt="" />
       <Heading>
-        Welcome,
+        <Image
+          src={Logo}
+          alt={"Logo"}
+          width={25}
+          height={25}
+          display={"inline-block"}
+          mr={1}
+        />
+        Welcome,{" "}
         <Text display="inline" color={"White"}>
           {userName}!
         </Text>
